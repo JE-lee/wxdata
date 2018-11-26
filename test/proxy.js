95e3375d41064e969e1d57f64d386a7a7d7060fc
JE_lee <1245183029@qq.com>
2018-11-26 22:04:04 +0800

    改为使用mocha来测试

--- test/proxy.js
+++ test/proxy.js
@@ -1,6 +1,8 @@
 import _proxy  from '../src/proxy'
 import _ from 'lodash'
 
+const assert = require('assert')
+
 let context = {
   data: {
     name: 'lee',
@@ -18,44 +20,41 @@ let context = {
   }
 }
 
+describe('proxy', function(){
+  it('proxy data 1', function(done){
+    this.timeout(1000)
+    let origin = _.cloneDeep(context)
+    _proxy(origin, origin.data, (data) => {
+      assert.equal(data.name, 'limei', 'data.name 应该是limei')
+      assert.equal(data.info.year, '2002', 'data.info.year 应该是2002')
+      assert.equal(data.pets[0], 'liming', 'data.pets[0] 应该是liming')
+      assert.equal(data.pets[1].name, 'kiki', 'data.pets[1].name 应该是kiki')
+      assert.equal(data.pets[2], 'vikey', 'data.pets[2] 应该是vikey')
+      done()
+    })
 
-QUnit.test(`_proxy`, function(assert){
-  let done = assert.async()
-  assert.timeout(1000)
-
-  let origin = _.cloneDeep(context)
-  _proxy(origin, origin.data, (data) => {
-    assert.equal(data.name, 'limei', 'data.name 应该是limei')
-    assert.equal(data.info.year, '2002', 'data.info.year 应该是2002')
-    assert.equal(data.pets[0], 'liming', 'data.pets[0] 应该是liming')
-    assert.equal(data.pets[1].name, 'kiki', 'data.pets[1].name 应该是kiki')
-    assert.equal(data.pets[2], 'vikey', 'data.pets[2] 应该是vikey')
-    done()
-  })
-  origin.$data.name = 'limei'
-  origin.$data.info.year = '2002'
-  origin.$data.pets[0] = 'liming'
-  origin.$data.pets[1].name = 'kiki'
-  origin.$data.pets.push('vikey')
-})
-
-QUnit.test('_proxy1', function(assert){
-  let done = assert.async()
-  assert.timeout(1000)
-  
-  let origin = _.cloneDeep(context)
-  _proxy(origin, origin.data, (data) => {
-    console.log('finish')
-    assert.equal(data.info.year, '2012', 'data.info.year应该是2012')
-    assert.equal(data.info.graduate, undefined, 'data.info.graduate 应该是undefind')
-    assert.equal(data.pets[1], '123', 'data.pets[1]应该是123')
-    done()
+    origin.$data.name = 'limei'
+    origin.$data.info.year = '2002'
+    origin.$data.pets[0] = 'liming'
+    origin.$data.pets[1].name = 'kiki'
+    origin.$data.pets.push('vikey')
   })
 
-  origin.$data.info = {
-    year: '2012'
-  }
-  origin.$data.pets[1] = '123'
-})
+  it('proxy data 2', function(done){
+    this.timeout(1000)
+    
+    let origin = _.cloneDeep(context)
+    _proxy(origin, origin.data, (data) => {
+      assert.equal(data.info.year, '2012', 'data.info.year应该是2012')
+      assert.equal(data.info.graduate, undefined, 'data.info.graduate 应该是undefind')
+      assert.equal(data.pets[1], '123', 'data.pets[1]应该是123')
+      done()
+    })
 
+    origin.$data.info = {
+      year: '2012'
+    }
+    origin.$data.pets[1] = '123'
+  })
+})
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,10 @@
 const path = require('path')
 module.exports = {
   entry: {
-    index: './src/index.js',
-    'test-proxy': './test/proxy.js'
+    index: './src/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
-  },
-  module: {
-    rules: [
-      { test: /\.js$/, use: 'babel-loader' }
-    ]
   }
 }
